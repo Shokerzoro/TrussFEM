c7769327964e6baeeceaccff16e7d1467e93894a
Shokerzoro <94473752+Shokerzoro@users.noreply.github.com>
2023-12-09 19:31:47 +0300

    Add files via upload

---
+++ counters.c
@@ -0,0 +1,35 @@
+/* Глобальный счетчик элементов.
+    Обеспечивает уникальность создаваемых структур.
+*/
+
+extern unsigned get_point_id(void);
+extern unsigned get_section_properties_id(void);
+extern unsigned get_truss_finite_element_id(void);
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+struct point_list{
+    unsigned id;
+    struct node* next_node;
+};
+
+struct {
+unsigned point_id_counter;
+unsigned section_properties_id_counter;
+unsigned truss_finite_element_id_counter;
+} global_counter = {0, 0, 0};
+
+extern unsigned get_point_id(void)
+{
+    global_counter.point_id_counter ++;
+    return global_counter.point_id_counter;
+}
+extern unsigned get_section_properties_id(void)
+{
+    global_counter.section_properties_id_counter ++;
+    return global_counter.section_properties_id_counter;
+}
+extern unsigned get_truss_finite_element_id(void)
+{
+    global_counter.truss_finite_element_id_counter ++;
+    return global_counter.truss_finite_element_id_counter;
+}
---
+++ structs.c
@@ -0,0 +1,42 @@
+/* Содержит структуру КЭ и функции для его создания
+*/
+/* - - - - - - - Предоставляемые функции - - - - - - - - */
+extern void initialize_FEM_data(void);
+extern void create_point (double x, double y);
+extern void create_section (unsigned section_properties_id, double elastity, double max_stress, double square, double enertion);
+extern void create_truss_FE_join (unsigned start_point_id, unsigned end_point_id, unsigned section_id);
+extern void create_truss_FE_pull(unsigned start_point_id, double x, double y, unsigned section_id);
+/* - - - - - - - Внешние функии - - - - - - - - - - - - - - - - - */
+unsigned get_point_id(void);
+unsigned get_section_properties_id(void);
+unsigned get_truss_finite_element_id(void);
+/* - - - - - - - - Структуры - - - - - -  - - - - - - - - - - - - - - - -*/
+
+typedef struct point{
+unsigned point_id;
+double x;
+double y;
+} POINT;
+
+typedef struct section{
+unsigned section_id;
+double elastity;
+double max_stress;
+double square;
+double enertion;
+} SECTION;
+
+typedef struct truss_finite_element{
+unsigned truss_id;
+POINT* start;
+POINT* end;
+SECTION* section;
+} TrussFE;
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+struct point* points_list;
+struct section* section_list;
+struct truss_finite_element* trussFE_list;
+
+
